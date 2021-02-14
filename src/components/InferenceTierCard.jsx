2d28afb3c43c2f57b170cdc9514cc13718e1a0eb
kasperni98 <kasper.nilssen1@gmail.com>
2021-02-14 14:52:03 +0100

    add inference texts to inference tier list

--- src/components/InferenceTierCard.jsx
+++ src/components/InferenceTierCard.jsx
@@ -6,10 +6,10 @@ class InferenceTier extends Component {
         return(
             <div className="InferenceTierCard">
                 <header className="InferenceTierCardHeader">
-                    <h1 className="InferenceTierCardName">{ this.props.tier_name }</h1>
+                    <h1 className="InferenceTierCardTierName">{ this.props.tier_name }</h1>
                 </header>
                 <div className="InferenceTierCardBody">
-                    { this.props.inferences.map((inference, id) => <div className="InferenceTierCardText" key={ id }>{inference.name}<div className="InferenceTierCardPlayer"> <audio controls><source src={ inference.audio_url }></source></audio></div></div> )}
+                    { this.props.inferences.map((inference, id) => <div><div className="InferenceTierCardInferenceName" key={ id }>{ inference.name }</div> <div className="InferenceTierCardInferenceText"> "{ inference.text }" </div> <div className="InferenceTierCardPlayer"><audio controls><source src={ inference.audio_url }></source></audio></div></div> )}
                 </div>
             </div>
         )
