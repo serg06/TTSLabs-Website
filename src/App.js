2e701d1ff9946338a5cfde0816d86e9d6826d516
kasperni98 <kasper.nilssen1@gmail.com>
2021-02-14 02:00:45 +0100

    add logo, fix sizing for containers

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './style.css'
+import logo from "./images/logo.png"
 
 import TierList from "./components/TierList"
 import InferenceTierList from "./components/InferenceTierList"
@@ -7,7 +8,7 @@ import InferenceTierList from "./components/InferenceTierList"
 function App() {
   return (
     <div className="App">
-      <header className="title">TTSLabs</header>
+      <img className="logo" src={ logo } alt="Logo"></img>
       <section className="TierList">
         <header className="TierListHeader">
           Our Plans
@@ -21,7 +22,7 @@ function App() {
         <header className="InferenceTierListHeader">
           Inferences
           <p1 className="InferenceTierListHeaderText">
-          Yeah I read that too. Are you gonna plagiarize the whole thing for us? Do you have any thoughts of your own on this matter? Is that your thing? You come into a thread, you copy some obscure passage and then paste it off as your own idea just to impress some of your "crew" and downvote my friend? See the sad thing about a guy like you is that in 50 years you're gonna start doing some thinking on your own and you're gonna come up with the fact that there are two certainties in life One, you are a talentless and unoriginal hack. And two, you wasted time trying to post other people's thoughts as your own while you could've been honing your skills to be actually creative instead of a thoughtless loser with a paste fetish.
+            Yeah I read that too. Are you gonna plagiarize the whole thing for us? Do you have any thoughts of your own on this matter? Is that your thing? You come into a thread, you copy some obscure passage and then paste it off as your own idea just to impress some of your "crew" and downvote my friend? See the sad thing about a guy like you is that in 50 years you're gonna start doing some thinking on your own and you're gonna come up with the fact that there are two certainties in life One, you are a talentless and unoriginal hack. And two, you wasted time trying to post other people's thoughts as your own while you could've been honing your skills to be actually creative instead of a thoughtless loser with a paste fetish.
           </p1>
         </header>
         <InferenceTierList/>
